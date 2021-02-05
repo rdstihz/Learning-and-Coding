7974e64e3e6466723cb631a7575109b2b67c67bc
rdstihz <rdstihz@qq.com>
2021-02-05 15:38:19 +0800

    ACW166

--- src/LYD-CH3/ACW166.cpp
+++ src/LYD-CH3/ACW166.cpp
@@ -1,94 +1,94 @@
-#include <cstdio>
-#include <cstring>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
+
 const int N = 9;
-const int maxn = 90;
 
-char s[maxn];
-int num[N][N];
+int ones[1 << N], map[1 << N];
+char str[100];
 
-int used_row[N];
-int used_col[N];
-int used_pal[N];
+int row[N], col[N], cell[3][3];
 
-int X[maxn], Y[maxn], q[maxn], poss[maxn];
-int n;
-
-bool dfs(int cur) {
-    if (cur > n) {
-        return true;
-    }
-
-    int i = X[q[cur]], j = Y[q[cur]];
-    int p = i / 3 * 3 + j / 3;
-    for (int k = 1; k <= N; k++) {
-        if (!((used_row[i] >> k & 1) || (used_col[j] >> k & 1) || (used_pal[p] >> k & 1))) {
-            num[i][j] = k;
-            used_row[i] ^= 1 << k;
-            used_col[j] ^= 1 << k;
-            used_pal[p] ^= 1 << k;
-
-            if (dfs(cur + 1)) return true;
-
-            used_row[i] ^= 1 << k;
-            used_col[j] ^= 1 << k;
-            used_pal[p] ^= 1 << k;
-        }
-    }
-
-    return false;
+inline int lowbit(int x) {
+    return x & (-x);
 }
 
-bool cmp(int a, int b) {
-    return poss[a] < poss[b];
+void init() {
+    for (int i = 0; i < N; i++)
+        col[i] = row[i] = (1 << N) - 1;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            cell[i][j] = (1 << N) - 1;
+}
+
+int get(int x, int y) {
+    return row[x] & col[y] & cell[x / 3][y / 3];
+}
+
+bool dfs(int cnt) {
+    if (!cnt) return true;
+
+    //1.找到选择最少的格子
+    int minv = 10;
+    int x, y;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            if (str[i * 9 + j] == '.') {
+                if (ones[get(i, j)] < minv) {
+                    minv = ones[get(i, j)];
+                    x = i, y = j;
+                }
+            }
+    for (int i = get(x, y); i; i -= lowbit(i)) {
+        int t = map[lowbit(i)];
+
+        str[x * 9 + y] = t + '1';
+        row[x] ^= 1 << t;
+        col[y] ^= 1 << t;
+        cell[x / 3][y / 3] ^= 1 << t;
+
+        if (dfs(cnt - 1)) return true;
+
+        str[x * 9 + y] = '.';
+        row[x] ^= 1 << t;
+        col[y] ^= 1 << t;
+        cell[x / 3][y / 3] ^= 1 << t;
+    }
+    return false;
 }
 
 int main() {
 
-    while (scanf("%s", s), s[0] != 'e') {
+    for (int i = 0; i < N; i++)
+        map[1 << i] = i;
+    for (int i = 0; i < 1 << N; i++) {
+        int s = 0;
+        for (int j = i; j; j -= lowbit(j))
+            s++;
+        ones[i] = s;
+    }
 
-        n = 0;
-        memset(used_row, 0, sizeof(used_row));
-        memset(used_col, 0, sizeof(used_col));
-        memset(used_pal, 0, sizeof(used_pal));
+    while (cin >> str, str[0] != 'e') {
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (s[9 * i + j] == '.') {
-                    n++;
-                    X[n] = i, Y[n] = j;
-                    q[n] = n;
-                } else {
-                    int k = s[9 * i + j] - '0';
-                    num[i][j] = k;
-                    used_row[i] |= 1 << k;
-                    used_col[j] |= 1 << k;
-                    used_pal[i / 3 * 3 + j / 3] |= 1 << k;
-                }
+        init();
+        int cnt = 0;
+        for (int i = 0, k = 0; i < N; i++) {
+            for (int j = 0; j < N; j++, k++) {
+                if (str[k] != '.') {
+                    int t = str[k] - '1';
+                    row[i] ^= 1 << t;
+                    col[j] ^= 1 << t;
+                    cell[i / 3][j / 3] ^= 1 << t;
+
+                } else
+                    cnt++;
             }
         }
 
-        for (int t = 1; t <= n; t++) {
-            int i = X[t], j = Y[t];
-            int p = i / 3 * 3 + j / 3;
-            for (int k = 1; k <= N; k++) {
-                if (!((used_row[i] >> k & 1) || (used_col[j] >> k & 1) || (used_pal[p] >> k & 1))) {
-                    poss[t]++;
-                }
-            }
-        }
+        dfs(cnt);
 
-        sort(q + 1, q + 1 + n, cmp);
-        dfs(1);
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                printf("%d", num[i][j]);
-            }
-        }
-        printf("\n");
+        cout << str << endl;
     }
 
     return 0;
