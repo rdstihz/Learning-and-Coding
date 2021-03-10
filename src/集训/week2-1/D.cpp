af0a873347c3186f7013f36e83c673ae678ae656
rdstihz <rdstihz@qq.com>
2021-03-10 12:34:54 +0800

    集训week2-1

--- src/集训/week2-1/D.cpp
+++ src/集训/week2-1/D.cpp
@@ -8,8 +8,8 @@ const LL P = 1e9 + 7;
 const int maxn = 1010;
 const int maxm = 210;
 
-LL f[2][maxm][maxm];
-LL g[2][maxm][maxm];
+LL f[2][maxm][maxm]; // f(i, j, k)： a串前i个字符，与b串前j个字符匹配，用了k个子串，的方案数
+LL g[2][maxm][maxm]; // g(i, j, k): a[i]与b[j]匹配,用了k个子串，的方案数
 
 char a[maxn], b[maxm];
 
@@ -22,7 +22,9 @@ int main() {
     f[0][0][0] = 1;
     int p = 0;
     for (int i = 1; i <= n; i++) {
-        f[p ^ 1][0][0] = 1;
+        //上一行： p
+        //当前行: p ^ 1
+        f[p ^ 1][0][0] = 1; //边界: f(i, 0, 0) = 1
         for (int j = 1; j <= m; j++) {
             for (int k = 1; k <= q; k++) {
                 if (a[i] != b[j]) {
@@ -34,7 +36,7 @@ int main() {
                 }
             }
         }
-        p ^= 1;
+        p ^= 1; //更换当前行
     }
     printf("%lld\n", f[p][m][q]);
     return 0;
