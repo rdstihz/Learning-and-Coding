82fe740ab166c59cea2220f4237ac10dfacda18d
rdstihz <rdstihz@qq.com>
2021-05-09 22:12:42 +0800

    2021 年东华大学金马程序设计联赛

--- src/EOJ/20210509/G.cpp
+++ src/EOJ/20210509/G.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 const int P = 998244353;
 const int maxn = 5001;
-int dp[2][maxn][maxn];
-
-long long g[maxn];
+int dp[maxn][maxn];
 
 int fir[maxn], nxt[2 * maxn];
 int to[2 * maxn];
@@ -21,45 +18,82 @@ void add_edge(int a, int b) {
     fir[a] = tot;
 }
 
-void dfs(int u, int f) {
-    int v;
-    for (int e = fir[u]; e; e = nxt[e]) {
-        v = to[e];
-        if (v != f) {
-            dfs(v, u);
-        }
+inline void add(int& a, int b) {
+    a = (a + b) % P;
+}
+
+int mul(int a, int b) {
+    int res = 0;
+    while (b) {
+        if (b & 1)
+            add(res, a);
+        a = (a + a) % P;
+        b >>= 1;
     }
+    return res;
+}
 
-    dp[0][u][0] = 1;
-    dp[1][u][1] = 1;
+int pow_mod(int a, int b, int p) {
+    int res = 1;
+    while (b) {
+        if (b & 1)
+            res = mul(res, a);
+        b >>= 1;
+        a = mul(a, a);
+    }
+    return res;
+}
+
+int dfs(int u, int f) {
+
+    int sz = 1;
+
+    dp[u][1] = 1;
+    
 
     for (int e = fir[u]; e; e = nxt[e]) {
-        v = to[e];
+        int v = to[e];
         if (v != f) {
-
-            for (int s = 0; s <= n; s++)
-                add(dp[0][u][s], dp[0][v][s]);
-            
-            for(int s = 2; s <= n; s++) {
-                g[s] = 
+            int t = dfs(v, u);
+            sz += t;
+            for (int j = sz; j >= 2; j--) {
+                for (int k = j - t; k < j; k++) {
+                    add(dp[u][j], mul(dp[u][k], dp[v][j - k]));
+                }
             }
-        
         }
     }
+
+    return sz;
 }
 
 int main() {
 
-    cin >> n;
+    while (cin >> n) {
+        int v;
+        memset(fir, 0, sizeof(fir));
+        memset(dp, 0, sizeof(dp));
+        tot = 0;
+        for (int u = 2; u <= n; u++) {
+            cin >> v;
+            add_edge(u, v);
+            add_edge(v, u);
+        }
 
-    int v;
-    for (int u = 2; u <= n; u++) {
-        cin >> u;
-        add_edge(u, v);
-        add_edge(v, u);
+        dfs(1, -1);
+
+        int a = 0, b = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                //a = (a + j * dp[i][j] % P) % P;
+                add(a, mul(j, dp[i][j]));
+                //b = (b + dp[i][j]) % P;
+                add(b, dp[i][j]);
+            }
+        }
+        cout << a * pow_mod(b, P - 2, P) % P << endl;
     }
-
-    dfs(1, -1);
 
     return 0;
 }
--- src/EOJ/20210509/J.cpp
+++ src/EOJ/20210509/J.cpp
@@ -10,14 +10,14 @@ bool vis[maxn];
 
 int main() {
 
-    int n = 1000;
+    int n = 500;
     sg[0] = 0;
 
     for (int x = 1; x <= n; x++) {
         memset(vis, 0, sizeof(vis));
         for (int i = 1; i <= x; i++) {
-            for (int k = 1; k <= 3 && i + k - 1 <= x; k++) {
-                vis[sg[i - 1] ^ sg[x - i - k + 1]] = true;
+            for (int k = 0; k <= 3 && i + k - 1 <= x; k++) {
+                vis[sg[i - 1] ^ sg[x - (i + k)]] = true;
             }
         }
         sg[x] = 0;
@@ -25,42 +25,25 @@ int main() {
             sg[x]++;
     }
 
-    for(int i = 1; i <= 1000; i++) {
-        if(sg[i] == 88)
-            cout << i << endl;
-    }
+    for(int i = n + 1; i <= 1000000; i++)
+        sg[i] = sg[i - 24];
+
     
-    // while(cin >> n) {
-    //     int res = 0;
-    //     int v;
+    while(cin >> n) {
+        int res = 0;
+        int v;
 
-    //     for(int i = 1; i <= n; i++) {
-    //         cin >> v;
-    //         res ^= sg[v];
-    //     }
+        for(int i = 1; i <= n; i++) {
+            cin >> v;
+            res ^= sg[v];
+        }
 
-    //     if(res == 0) {
-    //         cout << "lets see who is real evil genius" << endl;
-    //     }else{
-    //         cout << "Emo:?" << endl;
-    //     }
-    // }
+        if(res == 0) {
+            cout << "lets see who is real evil genius" << endl;
+        }else{
+            cout << "Emo:?" << endl;
+        }
+    }
 
     return 0;
 }
-
-/*
-2
-1 1
-1
-4
-1
-5
-1
-9
-3
-15 16 17
-3
-1000000 1 2
-
-*/
