5c7adcd6973aaf5283230b3c6d10077526e17061
rdstihz <rdstihz@qq.com>
2021-06-10 10:12:34 +0800

    Codeforces Round 724 div2

--- src/Codeforces/CF724/D.cpp
+++ src/Codeforces/CF724/D.cpp
@@ -5,7 +5,6 @@ using namespace std;
 const int maxn = 200000 + 100;
 
 int a[maxn];
-vector<int> nums;
 
 int main() {
 
@@ -15,32 +14,40 @@ int main() {
     while (T--) {
         int n;
         cin >> n;
+        int v;
 
-        nums.clear();
         for (int i = 1; i <= n; i++) {
             cin >> a[i];
-            nums.push_back(a[i]);
         }
 
-        sort(nums.begin(), nums.end());
-        nums.erase(unique(nums.begin(), nums.end()), nums.end());
-        for (int i = 1; i <= n; i++) {
-            a[i] = lower_bound(nums.begin(), nums.end(), a[i]) - nums.begin();
-        }
+        set<int> S;
+        S.insert(a[1]);
 
-
-
-        bool flag = false;
+        bool ok = true;
         for (int i = 2; i <= n; i++) {
-            int x = a[i - 1];
-            int y = a[i];
-            
+            if (a[i] == a[i - 1]) continue;
+            if (a[i] > a[i - 1]) {
+                auto it = S.upper_bound(a[i - 1]);
+                if (it != S.end() && *it < a[i]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (a[i] < a[i - 1]) {
+                auto it = S.lower_bound(a[i - 1]);
+                if (it != S.begin()) {
+                    it--;
+                    if (*it > a[i]) {
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+
+            S.insert(a[i]);
         }
 
-        if (flag)
-            cout << "NO" << endl;
-        else
-            cout << "YES" << endl;
+        cout << (ok ? "YES" : "NO") << endl;
     }
 
     return 0;
